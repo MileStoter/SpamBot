64678e068719fa0705efddf07bc5b7dd1489833d
MileStoter <73908429+MileStoter@users.noreply.github.com>
2020-11-03 19:52:24 +0000

    Add files via upload

---
+++ SpamBot/SpamBot.py
@@ -0,0 +1,14 @@
+import time, pyautogui #imports packets (don't chnage)
+time.sleep(3) #time before auto typing starts def:3
+f = open("num", 'r') #opens this file change this num to a file name you want it to read if its a txt file remember to put txt at the end
+for word in f: # ignore this (don't change)
+	pyautogui.typewrite(word) #types word (don't change)
+	time.sleep(1) #waits so it wont get kicked by discord also depends on the text def:1
+	pyautogui.press("enter") #presses enter (don't chnage)
+#Script made by Mile Stoter
+#Mile Storer#2868 on discord
+#https://www.facebook.com/mile.stoter.1
+#https://www.twitch.tv/milestoter
+#https://twitter.com/MileStoter
+#https://www.youtube.com/channel/UC91WGd4qhYojzx2CJWKL6Iw
+#https://www.reddit.com/user/MileStorer
